2df190991fd8999681b12a7257b4256aba0bd355
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-17 18:48:47 -0300

    imagenes proyectos

--- js/nav.js
+++ js/nav.js
@@ -6,3 +6,9 @@ $('.carousel.carousel-slider').carousel({
   fullWidth: true,
   autoplay: true
 });
+
+autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
