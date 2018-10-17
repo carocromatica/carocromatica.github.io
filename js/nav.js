c398ca7f8731632483181f732fe81eb16b4ae37b
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-17 19:37:10 -0300

    form en modal

--- js/nav.js
+++ js/nav.js
@@ -2,13 +2,9 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
 });
     
-$('.carousel.carousel-slider').carousel({
-  fullWidth: true,
-  autoplay: true
-});
 
-autoplay();
-function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 4500);
-}
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+      
